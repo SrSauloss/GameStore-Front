87ba8d035e42c3106f3485cc95e3716db69bec36
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-11-09 15:14:03 -0300

    created route to sign-in

--- src/services/API.js
+++ src/services/API.js
@@ -3,5 +3,6 @@ import axios from "axios";
 const API_URL = "https://projeto15-gamestore-back.herokuapp.com";
 
 const signUp = ({ body }) => axios.post(`${API_URL}/sign-up`, body);
+const signIn = ({ body }) => axios.post(`${API_URL}/sign-in`, body);
 
-export { signUp };
+export { signUp, signIn };
