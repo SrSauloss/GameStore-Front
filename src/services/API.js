08d3cb46057fe3d5a5e2e8c55ec3b2f63e48d48c
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-11-12 09:10:09 -0300

    created function to get product info

--- src/services/API.js
+++ src/services/API.js
@@ -5,4 +5,5 @@ const API_URL = "https://projeto15-gamestore-back.herokuapp.com";
 const signUp = ({ body }) => axios.post(`${API_URL}/sign-up`, body);
 const signIn = ({ body }) => axios.post(`${API_URL}/sign-in`, body);
 const listProducts = () => axios.get(`${API_URL}/product/all`);
-export { signUp, signIn, listProducts };
+const listProductInfo = ({ id }) => axios.get(`${API_URL}/product/${id}`);
+export { signUp, signIn, listProducts, listProductInfo };
