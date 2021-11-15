1e6e8d28e8dd64099e16bd395fca01a778f7aea2
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-11-15 16:26:03 -0300

    created test for login error

--- cypress/integration/2- signIn/signIn.spec.js
+++ cypress/integration/2- signIn/signIn.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /// <reference types="cypress" />
 
 describe("Login", () => {
@@ -9,5 +10,15 @@ describe("Login", () => {
     cy.contains("Sign In").click();
 
     cy.url().should("equal", "http://localhost:3000/products");
+  });
+
+  it("should show error message for invalid params", () => {
+    cy.visit("http://localhost:3000/sign-in");
+
+    cy.get("input[placeholder=e-mail]").type("a@a.com");
+    cy.get("input[placeholder=password").type("123456a!a");
+    cy.contains("Sign In").click();
+
+    cy.contains("Invalid e-mail or password!").should("be.visible");
   });
 });
